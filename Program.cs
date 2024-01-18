b0bbc636b2af3c0632156920744831f50aa24626
Leonardo buta <teste@teste.com>
2024-01-17 21:51:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using BaseDeUmTriangulo;
+using System;
+using System.Formats.Asn1;
+using System.Globalization;
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre Com As Medidias do Triangulo X ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areax = x.area();
+            double areay = y.area();
+
+
+            Console.WriteLine("Entre Com as Medidias do Trinagulo y ");
+            y.A = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+            y.B = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("a area de x " + areax.ToString("F4"));
+            Console.WriteLine("a area de y " + areay.ToString("F4"));
+
+            if (areax > areay)
+            {
+                Console.WriteLine("a maior area é x");
+            }
+            else
+            {
+                Console.WriteLine("a maior área é y");
+            }
+
+
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDeUmTriangulo
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+        }
+
+
+    }
+}
